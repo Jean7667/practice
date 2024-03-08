73e4ecbdaf07d02fed72b7bbf54ff9034346fb42
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-03-08 23:07:02 +0000

    completed basic css on going js to add and remove an active class

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+console.log("JSOK");
+
+// Add active class to the current button (highlight it)
+const allinks = document.querySelectorAll(".link")
+
+console.log(allinks);
+
+links.array.forEach(links => {
+    links
+    
+});
